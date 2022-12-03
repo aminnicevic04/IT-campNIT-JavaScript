b72856fa9265130e1046518e7774051ac53f57a1
aminnicevic04 <aminnicevic@gmail.com>
2022-12-03 17:03:34 +0100

    cas

---
+++ cas5 for petlja/app.js
@@ -0,0 +1,67 @@
+// for(i = 0; i < 5; i++){
+//     console.log(1);
+// }
+
+//ispisi brojeve od 1-10 unazad
+// for(i=10; i>0; i--){
+//     console.log(i);
+// }
+
+// for(i=0; i<=10; i++){
+//     console.log(i); 
+// }
+
+//Naci sumu prvih 10 prirodnih brojeva
+// suma=0; 
+// for(i=1;i<=10;i++){
+//     suma += i;
+// }
+// console.log(suma)
+
+// proizvod=1
+// // debugger;
+// for(i=1; i<=5; i++){
+// proizvod *=i
+// }
+
+// console.log(proizvod)
+
+//uneti broj n i saberi sumu od n-1 n?
+// n=20
+// suma=0;
+// for(i=0; i<=n; i++){
+//    suma += i;
+// }
+// console.log(i)
+
+//uneti deset brojeva i iyracunati sumu i avg vrednost
+// sumu=0;
+// prosek=0;
+// n=Number(prompt("uneti broj n1"))
+// for(i=0; i<10; i++){
+//     n=Number(prompt("uneti broj n" , i))
+//     sumu+=n;
+//     prosek = sumu/10;
+// }
+// console.log(sumu)
+
+//unesi na kub broj n u pet primera (prompt)
+// for(i=0;i<5;i++){
+//     broj=Number(prompt("unesi broj"));
+//     console.log(broj ** 3)
+// }
+
+//napisi tablicu mnozenja od 1-10 za neki broj
+
+// n=Number(prompt("unesite broj"));
+// for(i=1; 1<=10; i++){
+//     console.log(n,"*", i, "-->", n*i);
+// }
+
+//napravi pattern od zvezdica
+
+zvezdice="";
+for(i=0;i<5; i++){
+    zvezdice += "*";
+    console.log(zvezdice);
+}
