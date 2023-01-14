65ee90d04970b4395f5207022a461144149434ed
aminnicevic04 <aminnicevic@gmail.com>
2023-01-14 14:43:13 +0100

    cas

--- cas 16 test/app.js
+++ cas 16 test/app.js
@@ -1,11 +1,11 @@
 // zadatak 1
-// let broj = prompt("Unesite broj");
+let broj = prompt("Unesite broj");
 
-// if (isNaN(broj)) {
-//   console.log("Niste uneli broj");
-// } else {
-//   console.log(broj ** 2);
-// }
+if (isNaN(broj)) {
+  console.log("Niste uneli broj");
+} else {
+  console.log(broj ** 2);
+}
 
 // zadatak 2
 
@@ -31,3 +31,16 @@ for (let i = 0; i <= recenica.length; i++) {
   }
 }
 console.log(nova);
+
+// Zadatak 3
+let latice = prompt("unesite broj latica");
+niz = ["i love u", "a little", "a lot", "passionately", "madly", "not at all"];
+console.log(niz[(latice % niz.length) - 1]);
+
+// Zadatak 4
+var rezultat = 0;
+const niz = [1, 2, 3, 4, 5];
+for (i = 0; i < niz.length; i++) {
+  rezultat += niz[i] ** 2;
+}
+console.log(rezultat);
