ade5000d593c3ce3dc170dc37af8c33633b51fc1
aminnicevic04 <aminnicevic@gmail.com>
2023-01-17 23:13:55 +0100

    domaci

--- Domaci zadaci/domaci 6/app.js
+++ Domaci zadaci/domaci 6/app.js
@@ -2,16 +2,22 @@
 //Nadji najduzu rec u stringu.
 
 const str = "trenutno radim domaci zadatak";
-const newStr = str.split(" ");
-let string;
-let brojac = 0;
-let recenica = "";
-for (i = 0; i <= newStr.length - 1; i++) {
-  string = new String(newStr[i]);
-  if (string.length > brojac) {
-    brojac = string.length;
-    recinca = string;
-    console.log(recenica);
-  }
-}
-console.log(newStr);
+let rec = str.split(" ");
+
+let najduza = rec.reduce((a, b) => (a.length > b.length ? a : b));
+console.log(najduza);
+
+//nacin 2
+// const newStr = str.split(" ");
+// let string;
+// let brojac = 0;
+// let recenica = "";
+// for (i = 0; i <= newStr.length - 1; i++) {
+//   string = new String(newStr[i]);
+//   if (string.length > brojac) {
+//     brojac = string.length;
+//     recinca = string;
+//     console.log(recenica);
+//   }
+// }
+// console.log(newStr);
