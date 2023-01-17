6d5dfd0a0a707586c2f4f37ef876d4dc93d52267
aminnicevic04 <aminnicevic@gmail.com>
2023-01-17 19:41:45 +0100

    cas

---
+++ cas 17/app.js
@@ -0,0 +1,8 @@
+let arrayNumber = [10, 15, 5, 7];
+
+//pop() - uklanja poslednji element iz niza
+console.log(arrayNumber.pop());
+
+//push() - dodaje novi element na kraju niza
+arrayNumber.push(20);
+console.log(arrayNumber);
