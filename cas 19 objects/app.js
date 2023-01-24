be2019bf9241738136ea7dcbfb942da961ab4a03
aminnicevic04 <aminnicevic@gmail.com>
2023-01-24 20:55:45 +0100

    domaci

--- cas 19 objects/app.js
+++ cas 19 objects/app.js
@@ -5,10 +5,6 @@ const osoba = {
   ime: "Hasan",
 };
 
-function punoImeIPrezime(ime, prezime) {
-  return ime + " " + prezime;
-}
-
 console.log(osoba.ime);
 console.log(osoba["ime"]);
 
@@ -30,6 +26,56 @@ const auto = {
 };
 
 auto.gume = "Zimske gume";
-console.log(Object.values(auto));
+console.log(Object.values(auto)); //vraca niz vrednosti ovog objecta
 
 // console.log(typeof null) je object
+
+//reduce metoda
+let niz = [10, 5, 4, 3, 2, 1, 5];
+// reduce((prethodno, trenutno) => prethodno + trenutno.initialValue);
+let suma = niz.reduce(
+  (prethodno, trenutno) => prethodno + trenutno,
+  0 //0 je inicijalna vrednost tj. vrednost od koje pocinjemo operaciju
+);
+console.log(suma);
+
+// function saberi(prethodno, trenutno) {
+//   return prethodno + trenutno;
+// }
+let niz1 = [10, 5, 4];
+// reduce((prethodno, trenutno) => prethodno + trenutno.initialValue);
+
+let mnozenje = niz1.map((prethodno) => prethodno * 2);
+let suma1 = niz1.reduce((prethodno, trenutno) => prethodno + trenutno, 0);
+console.log(suma1);
+console.log(mnozenje);
+
+let mnozenje1 = niz1.reduce(
+  (prethodno, trenutno) => prethodno + trenutno * 2,
+  0
+);
+console.log(mnozenje1);
+
+const imena = [
+  "amar",
+  "amin",
+  "amin",
+  "hasan",
+  "lejla",
+  "dzenis",
+  "hamdija",
+  "maki",
+];
+
+const nalazenje = function (ime) {
+  if (imena.indexOf("lejla") === -1) {
+    return "sad ce lejla samo sto nije";
+  }
+  return ime.indexOf("lejla");
+};
+
+console.log(nalazenje(imena));
+
+const noviNiz = new Set(imena); // set ne dozvoljava duplikat
+console.log(noviNiz); // i vraca nam niz bez duplikata
+
