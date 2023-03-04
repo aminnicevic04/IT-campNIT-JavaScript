3d5d316747b4ca23e245ede6ace5a0e6cc491fb6
aminnicevic04 <aminnicevic@gmail.com>
2023-03-04 15:14:51 +0100

    cas

---
+++ cas 33/app.js
@@ -0,0 +1,5 @@
+// single tread jezik
+// dinamicly typed jezik
+// callstack -> web api -> callback queue -> event loop -> callstack [async JS-protocol]
+// sta je error handing?
+// sta radi try catch?
