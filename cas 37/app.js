b444485d16cbec39e791a551ace97b339c16a19f
aminnicevic04 <aminnicevic@gmail.com>
2023-03-15 19:37:26 +0100

    cas

---
+++ cas 37/app.js
@@ -0,0 +1,36 @@
+// let btn = document.querySelector("Button");
+// let box = document.querySelector(".main");
+// let boje = ["red", "black", "green", "purple"];
+
+// btn.addEventListener("click", () => {
+//   let randomColor = Math.floor(Math.random() * boje.length);
+//   box.style.backgroundColor = boje[randomColor];
+// });
+
+// vezba2
+
+// let main = document.querySelector(".main");
+// let broj = document.querySelector(".broj");
+// let btn = document.querySelector(".btn1");
+// let input = document.querySelector("input");
+
+// btn.addEventListener("click", () => {
+//   let randomNumber = Math.floor(Math.random() * 4);
+//   console.log(randomNumber);
+//   if (input.value === randomNumber) {
+//     alert("Cestitamo, pogodili ste broj");
+//   } else {
+//     alert("Pokusajte opet");
+//   }
+// });
+
+//iks oks
+let a1 = document.querySelector(".box a1");
+let a2 = document.querySelector(".box a2");
+let a3 = document.querySelector(".box a3");
+let b1 = document.querySelector(".box b1");
+let b2 = document.querySelector(".box b2");
+let b3 = document.querySelector(".box b3");
+let c1 = document.querySelector(".box c1");
+let c2 = document.querySelector(".box c2");
+let c3 = document.querySelector(".box c3");
